a7b78ac81446dbe1244e736ae3ce0616d7f5ae92
Stephen Watkin <ste@smashbang.co.uk>
2015-09-10 17:23:57 +0100

    misc. changes

--- money/contrib/django/forms/fields.py
+++ money/contrib/django/forms/fields.py
@@ -11,10 +11,12 @@ class MoneyField(forms.MultiValueField):
     """
 
     def label_from_currency(self, c):
-        return CURRENCY_LABEL % {'code': c.code, 'name': c.name}
+        return CURRENCY_LABEL % {'code': c.code, 'name': c.name,
+                                 'symbol': c.symbol}
 
     def __init__(self, choices=None, decimal_places=2, max_digits=12,
-                 default_currency=None, min_value=None, *args, **kwargs):
+                 default_currency=None, min_value=None, max_value=None,
+                 *args, **kwargs):
         # Note that we catch args and kwargs that must only go to one field
         # or the other. The rest of them pass onto the decimal field.
         choices = choices or list(( (u"%s" % (c.code,), self.label_from_currency(c)) for i, c in sorted(CURRENCY.items()) if c.code != 'XXX'))
@@ -23,14 +25,14 @@ class MoneyField(forms.MultiValueField):
 
         fields = (
             forms.DecimalField(*args, decimal_places=decimal_places,
-                               min_value=min_value,
+                               min_value=min_value, max_value=max_value,
                                max_digits=max_digits, **kwargs),
             forms.ChoiceField(choices=choices)
         )
-        
+
         if default_currency:
-			kwargs.update({'initial': Money('', default_currency)})
-            
+            kwargs.update({'initial': Money('', default_currency)})
+
         super(MoneyField, self).__init__(fields, *args, **kwargs)
 
     def compress(self, data_list):
